796295be8872a1515fcd2160ccba40ae5ad8de77
Jonathon-Harkness <jharkne@umich.edu>
2025-03-24 23:05:09 -0400

    delete unused py files

--- game.py
+++
@@ -1,9 +0,0 @@
-class Game:
-
-    def __init__(self, guild_id, ball_status, ball_value, throw_type, time_active, current_thrower):
-        self.guild_id = guild_id
-        self.ball_status = ball_status
-        self.ball_value = ball_value
-        self.throw_type = throw_type
-        self.time_active = time_active
-        self.current_thrower = current_thrower
