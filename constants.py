4a45eb8b68f6dc9ec6b7fe4f76b43608baea9670
Jonathon-Harkness <jharkne@umich.edu>
2025-03-28 01:38:20 -0400

    update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,18 @@
-token="token"
+from enum import Enum
+
+TOKEN = "TOKEN"
+THROW_MIN = -500
+THROW_MAX = 500
+THROW_TYPE = {'ALIVE', 'DEAD'}
+SPECIAL_THROWS = {'CHERRY BOMB'}
+
+
+class SpecialThrows(Enum):
+    CHERRY_BOMB = 'CHERRY BOMB'
+
+
+class BallStatus(Enum):
+    ACTIVE = 'ACTIVE'
+    INACTIVE = 'INACTIVE'
+
+
