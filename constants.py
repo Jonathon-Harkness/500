03fe95e757655b5590b60642b3a84657972831e4
Jonathon-Harkness <jharkne@umich.edu>
2025-04-05 21:38:06 -0400

    update constants

--- constants.py
+++ constants.py
@@ -1,14 +1,22 @@
 from enum import Enum
 
+env = "prod"
+
+db_host_dev = "devHost"
+db_username_dev = "user"
+db_password_dev = "pass"
+db_port_dev = 0000
+
+db_host_prod = "prodHost"
+db_username_prod = "user"
+db_password_prod = "pass"
+db_port_prod = 0000
+
+
 TOKEN = "TOKEN"
 THROW_MIN = -500
 THROW_MAX = 500
 THROW_TYPE = {'ALIVE', 'DEAD'}
-SPECIAL_THROWS = {'CHERRY BOMB'}
-
-
-class SpecialThrows(Enum):
-    CHERRY_BOMB = 'CHERRY BOMB'
 
 
 class BallStatus(Enum):
